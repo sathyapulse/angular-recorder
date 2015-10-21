7256e22422dcceb30e058259cb3d2c16d5aa41cd
Joseph Taiwo Orilogbon <logbon72@gmail.com>
2015-10-21 11:27:57 +0100

    Update demo.js

--- demo/demo.js
+++ demo/demo.js
@@ -8,7 +8,7 @@
     console.log("Loaded");
 
   }).config(function(recorderServiceProvider){
-      recorderServiceProvider.forceSwf(false);
+      recorderServiceProvider.forceSwf(true);
     });
 
 })();
